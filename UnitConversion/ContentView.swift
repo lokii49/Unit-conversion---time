ea4445674bfea6af7f729b3aae6f462040370671
Lokesh Pudari <lokesh.pudari@temenos.com>
2025-08-31 15:46:30 +0530

    Initial Commit

---
+++ UnitConversion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UnitConversion
+//
+//  Created by Lokesh Pudari on 31/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Lokesh Pudari on 31/08/25.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
