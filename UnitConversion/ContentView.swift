1557fe14d3c5e50ebd139d88ee09be39f0d05280
Lokesh Pudari <lokesh.pudari@temenos.com>
2025-08-31 20:55:21 +0530

    Unit conversion challange - time conversion in all formats

--- UnitConversion/ContentView.swift
+++ UnitConversion/ContentView.swift
@@ -8,14 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var inputValue = 0
+    @State private var inputUnit = "mins"
+    @State private var outputUnit = "sec"
+    @FocusState private var inputValueSelected: Bool
+    
+    var timeUnits = ["days","hours", "mins", "sec"]
+    
+    var inputInSec: Double {
+        var result = 0.0
+        let input = Double(inputValue)
+        switch inputUnit {
+        case "days":
+            result = ((input * 60.0) * 60.0) * 24.0
+        case "hours":
+            result = (input * 60.0) * 60.0
+        case "mins":
+            result = input * 60.0
+        default:
+            result = input
         }
-        .padding()
+        return result
+    }
+    
+    var outputValue: Double {
+        var result = 0.0
+        switch outputUnit {
+        case "days":
+            result = ((inputInSec/24.0)/60.0)/60.0
+        case "hours":
+            result = ((inputInSec)/60.0)/60.0
+        case "mins":
+            result = (inputInSec)/60.0
+        default:
+            result = inputInSec
+        }
+        return result
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("Input Units") {
+                    Picker("units", selection: $inputUnit) {
+                        ForEach(timeUnits, id: \.self){
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("Input Value") {
+                    TextField("input value", value: $inputValue, format: .number)
+                }
+                .keyboardType(.numberPad)
+                .focused($inputValueSelected)
+                
+                Section("Output Units") {
+                    Picker("outputUnits", selection: $outputUnit) {
+                        ForEach(timeUnits, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("Output Value") {
+                    Text(outputValue, format: .number)
+                }
+                
+            }
+            .navigationTitle("Time Conversion")
+            .toolbar {
+                if inputValueSelected {
+                    Button("Done") {
+                        inputValueSelected = false
+                    }
+                }
+            }
+        }
     }
 }
 
